57aa4610a4d77f80eae33dd56516dcf1cfba0175
Asaf Alkhasov <asafalkhasov@gmail.com>
2020-12-11 05:25:42 +0400

    all files include

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'nnw1vot8p9'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-awrp7z2o.eu.auth0.com',            // Auth0 domain
+  clientId: '4XZYy70DQ8w1LPtOeKR975BFqVpjlKzf',          // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
